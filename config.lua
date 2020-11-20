bfc851cd337176ebdaec0146e919cad3320afecf
Cloud-Developments <38671245+Cloud-Developments@users.noreply.github.com>
2020-11-20 11:26:31 -0600

    Commit Version 0.1

---
+++ config.lua
@@ -0,0 +1,19 @@
+config = {
+
+	-- Particle References: --
+	-- https://vespura.com/fivem/particle-list/ --
+	
+	-- Config Info: --
+	-- "dict" = The dictionary with the particle you want. --
+	-- "name" = The name of the particle you want. --
+	-- "scale" = The scale of the particle you want. (Default = 1.0) --
+	-- "coords" = Contains the x,y,z, coordinates for the particle. --
+	-- "x" = The "X" coordinate of the particle. --
+	-- "y" = The "Y" coordinate of the particle. --
+	-- "z" = The "Z" coordinate of the particle. --
+
+	particles = {
+    {dict = 'core', name = 'ent_amb_smoke_chicken', scale = 1.0, coords = {x = -2020.85, y = -1038.95, z = 1.00}},
+	{dict = 'core', name = 'ent_amb_smoke_foundry', scale = 1.0, coords = {x = -2069.53, y = -1023.06, z = 10.00}}
+	}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'Cloudy'
+description 'Particle Creator'
+version '0.1'
+
+client_scripts {
+	'config.lua',
+    'client.lua'
+}
