f9968d1070ceb86e35914f142dbd13963c43a80f
cloudy-develop <38671245+Cloud-Developments@users.noreply.github.com>
2021-01-03 17:36:36 -0600

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,14 +1,46 @@
 Citizen.CreateThread(function()
 	Citizen.Wait(1)
-	for _,particle in pairs(config.particles) do
+
+	for i,var0 in pairs(config.particles) do
+		table.insert(var0, { data = { distance = nil, enable = nil, handle = nil } })
+	end
+
+	while (true) do
 		
-		RequestNamedPtfxAsset(particle.dict)
-		while not HasNamedPtfxAssetLoaded(particle.dict) do
-			Citizen.Wait(1)
+		local playerPed = PlayerPedId()
+		local playerCoords = GetEntityCoords(playerPed)
+
+		for i,var0 in pairs(config.particles) do
+
+			var0[1].data.distance = GetDistanceBetweenCoords(playerCoords, var0.coords.x, var0.coords.y, var0.coords.z, true)
+			
+			if (var0[1].data.distance < 800.0) then
+				if not (var0[1].data.enable) then
+
+					RequestNamedPtfxAsset(var0.dict)
+					while not HasNamedPtfxAssetLoaded(var0.dict) do
+						Citizen.Wait(1)
+					end
+
+					UseParticleFxAssetNextCall(var0.dict)
+
+					var0[1].data.handle = StartParticleFxLoopedAtCoord(var0.name, var0.coords.x, var0.coords.y, var0.coords.z, 0.0, 0.0, 0.0, var0.scale, false, false, false, 0)
+					var0[1].data.enable = true
+
+				end
+			else
+				if (var0[1].data.enable) then
+
+					StopParticleFxLooped(var0[1].data.handle, false)
+
+					var0[1].data.handle = nil
+					var0[1].data.enable = false
+
+				end
+			end
+
 		end
-		
-		UseParticleFxAssetNextCall(particle.dict)
-		StartParticleFxLoopedAtCoord(particle.name, particle.coords.x, particle.coords.y, particle.coords.z, 0.0, 0.0, 0.0, particle.scale, false, false, false, 0)
-	
+
+		Citizen.Wait(500)
 	end
 end)
